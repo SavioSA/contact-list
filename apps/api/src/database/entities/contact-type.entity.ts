519d2f8a7744e0b88c11f0e1296748df8368b4a1
saviosa <saviosilvadearaujo2019@gmaiil.com>
2022-08-01 15:45:56 -0300

    feat(contact-type.entity.ts): add contact-type entity

---
+++ apps/api/src/database/entities/contact-type.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import Contact from './contact.entity';
+
+@Entity({
+
+})
+export default class ContactType {
+    @PrimaryGeneratedColumn()
+      id: number;
+
+    @Column()
+      type: string;
+
+    @ManyToOne(() => Contact, (contact) => contact.contactTypes)
+      contacts: Contact[];
+}
