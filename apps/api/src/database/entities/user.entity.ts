2aafdcd04ebef285e97393186e0b2b76e604bd1e
saviosa <saviosilvadearaujo2019@gmaiil.com>
2022-08-01 14:50:54 -0300

    feat(user.entity.ts): create user entity

---
+++ apps/api/src/database/entities/user.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import Contact from './contact.entity';
+
+@Entity()
+export default class User {
+    @PrimaryGeneratedColumn()
+      id: number;
+
+    @Column()
+      name: string;
+
+    @OneToMany(() => Contact, (contact) => contact.user)
+    contacts: Contact[]
+}
