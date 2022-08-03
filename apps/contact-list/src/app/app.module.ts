d4e1e108f06ed94604dbbcbb031d5f5422763c5f
saviosa <saviosilvadearaujo2019@gmaiil.com>
2022-08-03 10:18:24 -0300

    feat(app.module.ts): enable external library

--- apps/contact-list/src/app/app.module.ts
+++ apps/contact-list/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { ContactListComponent } from './views/contact-list/contact-list.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, ContactListComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
