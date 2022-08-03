2e5341d36c65cba75d3f417a60729c50547b775e
saviosa <saviosilvadearaujo2019@gmaiil.com>
2022-08-03 10:41:24 -0300

    feat(material.module.ts): add material module

--- apps/contact-list/src/app/app.module.ts
+++ apps/contact-list/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
-import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ManagementButtonsComponent } from './components/management-buttons/management-buttons.component';
+import { MaterialModule } from "./material.module";
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { ContactListComponent } from './views/contact-list/contact-list.component';
 
-
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, ContactListComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatListModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    ContactListComponent,
+    ManagementButtonsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
