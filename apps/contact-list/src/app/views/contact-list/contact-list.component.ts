f0e45776cf89945ceeaf2ee4e6bdd5499b2882db
saviosa <saviosilvadearaujo2019@gmaiil.com>
2022-08-03 12:37:26 -0300

    feat(contact-list.component.ts): implement pagination

--- apps/contact-list/src/app/views/contact-list/contact-list.component.ts
+++ apps/contact-list/src/app/views/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import UserPaginationInterface from "../../interfaces/user-pagination.interface";
 import UserInterface from "../../interfaces/user.interface";
 import { UserService } from '../../services/user.service';
@@ -11,11 +12,24 @@ export class ContactListComponent implements OnInit {
   constructor(private userService: UserService) {}
 
   users: UserInterface[] = [] ;
-
+  pagesQuantity = 0;
+  totalItems = 0;
   ngOnInit(): void {
     this.userService.getAll(8).subscribe((res: UserPaginationInterface) => {
-      const { users } = res;
+      const { users, pagesQuantity, totalItems } = res;
       this.users = users;
+      this.pagesQuantity = pagesQuantity;
+      this.totalItems = totalItems;
+    })
+  }
+  changeIndex(pageEvent: PageEvent) {
+    console.log(pageEvent.pageSize);
+
+    this.userService.getAll(8, pageEvent.pageIndex+1).subscribe((res: UserPaginationInterface) => {
+      const { users, pagesQuantity, totalItems } = res;
+      this.users = users;
+      this.pagesQuantity = pagesQuantity;
+      this.totalItems = totalItems;
     })
   }
 }
