3ba6710bd29308942d12f261ff00f1064401b9bd
saviosa <saviosilvadearaujo2019@gmaiil.com>
2022-08-04 16:38:34 -0300

    feat(dialog-confirmation.component): add general dialog

--- apps/contact-list/src/app/app.module.ts
+++ apps/contact-list/src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DialogConfirmationComponent } from "./components/dialog-confirmation/dialog-confirmation.component";
 import { ManagementButtonsComponent } from './components/management-buttons/management-buttons.component';
 import { MaterialModule } from './material.module';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { UserService } from './services/user.service';
 import { ContactEditorModule } from './views/contact-editor/contact-editor.module';
 import { ContactListModule } from './views/contact-list/contact-list.module';
-
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, ManagementButtonsComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    ManagementButtonsComponent,
+    DialogConfirmationComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
