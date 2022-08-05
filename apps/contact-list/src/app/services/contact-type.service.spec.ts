9faffbae11723829963db45c4330cd9256752d4c
saviosa <saviosilvadearaujo2019@gmaiil.com>
2022-08-05 13:51:41 -0300

    feat(contact-type.service.spec.ts): add contact-type unit tests

--- apps/contact-list/src/app/services/contact-type.service.spec.ts
+++ apps/contact-list/src/app/services/contact-type.service.spec.ts
@@ -1,16 +1,58 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ContactTypeService } from './contact-type.service';
 
 describe('ContactTypeService', () => {
   let service: ContactTypeService;
+  let controller: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [
+        {
+          provide: MatSnackBar,
+          useValue: {
+            open: jest.fn,
+          },
+        },
+      ],
+    });
+    controller = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ContactTypeService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return contact types', () => {
+    service.getAll().subscribe((res) => {
+      expect(res).toEqual([
+        {
+          id: 1,
+          type: 'phone',
+        },
+      ]);
+    });
+  });
+
+  it('should return error to get contact types', () => {
+    service.getAll().subscribe({
+      next: () => {},
+      error: (error) => {
+        expect(error).toBeTruthy();
+      },
+    });
+
+    const req = controller.expectOne(
+      'http://localhost:3333/api/v1/contact-type?offset=0&page=0'
+    );
+    req.flush('error', { status: 500, statusText: 'Broken Service' });
   });
 });
--- apps/contact-list/src/app/services/contact.service.spec.ts
+++ apps/contact-list/src/app/services/contact.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ContactService } from './contact.service';
 
 describe('ContactService', () => {
