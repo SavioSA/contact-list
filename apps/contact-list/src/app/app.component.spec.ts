d06a91c97355fc519a22c94cbf4bcd65d111a340
saviosa <saviosilvadearaujo2019@gmaiil.com>
2022-08-05 14:43:09 -0300

    feat(app.component.spec.ts): remove unused test

--- apps/contact-list/src/app/app.component.spec.ts
+++ apps/contact-list/src/app/app.component.spec.ts
@@ -20,13 +20,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('contact-list');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome contact-list'
-    );
-  });
 });
