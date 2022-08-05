d77cb432faa6e6f871d52d1eaf1cb1ed9ca857f1
saviosa <saviosilvadearaujo2019@gmaiil.com>
2022-08-04 22:45:37 -0300

    feat(dialog-contact-editor): add dialog contact editor unit tests

--- apps/contact-list/src/app/views/contact-editor/components/dialog/dialog-contact-editor.spec.ts
+++ apps/contact-list/src/app/views/contact-editor/components/dialog/dialog-contact-editor.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogContactEditorComponent } from './dialog-contact-editor.component';
-
 
 describe('DialogComponent', () => {
   let component: DialogContactEditorComponent;
@@ -9,14 +9,70 @@ describe('DialogComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DialogContactEditorComponent],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {
+            close: jest.fn(),
+          },
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DialogContactEditorComponent);
     component = fixture.componentInstance;
+    component.data = {
+      formData: {
+        controls: {
+          phone: {
+            setValue: jest.fn(),
+          },
+          email: {
+            setValue: jest.fn(),
+          },
+        },
+      },
+    };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should clear form fields', () => {
+    const spy = jest.spyOn(
+      component.data.formData.controls['phone'],
+      'setValue'
+    );
+    const spy2 = jest.spyOn(
+      component.data.formData.controls['email'],
+      'setValue'
+    );
+    component.clearFields();
+    expect(spy).toBeCalledWith('');
+    expect(spy2).toBeCalledWith('');
+  });
+
+  it('should submit', () => {
+    jest.spyOn(component.dialogRef, 'close');
+    component.submit();
+    expect(component.dialogRef.close).toHaveBeenCalled;
+  });
+
+  it('should confirm is number', () => {
+    const spy = jest.spyOn(component, 'isNumber');
+    component.isNumber({ which: 30 });
+    expect(spy).toReturnWith(true);
+  });
+
+  it('should negate is number', () => {
+    const spy = jest.spyOn(component, 'isNumber');
+    component.isNumber({ which: 32 });
+    expect(spy).toReturnWith(false);
   });
 });
