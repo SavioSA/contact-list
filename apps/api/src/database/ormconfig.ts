9b83dc4bb222a73579d97b55e04cb53e4fabbbee
saviosa <saviosilvadearaujo2019@gmaiil.com>
2022-08-06 20:38:13 -0300

    feat(ormconfig.ts): fix docker migrations path

--- apps/api/src/database/ormconfig.ts
+++ apps/api/src/database/ormconfig.ts
@@ -5,6 +5,10 @@ import { DataSourceOptions } from 'typeorm';
 dotenv.config({ path: './.env' });
 
 export default function getConfig(host = process.env.MYSQL_MIGRATION_HOST) {
+
+console.log(resolve(`${join(__dirname)}/migration/**`));
+
+
   return {
     type: 'mysql',
     port: parseInt(process.env.MYSQL_PORT as string, 10),
@@ -16,7 +20,7 @@ export default function getConfig(host = process.env.MYSQL_MIGRATION_HOST) {
     synchronize: false,
     name: 'default',
     entities: [resolve(`${join(__dirname)}/entities/*.js`)],
-    migrations: host === process.env.MYSQL_MIGRATION_HOST ? [join(__dirname, '../../../../dist/apps/api/src/database/migrations/**')] : [resolve(`${join(__dirname)}/migration/*.js`)],
+    migrations: host === process.env.MYSQL_MIGRATION_HOST ? [join(__dirname, '../../../../dist/apps/api/src/database/migrations/**')] : [resolve(`${join(__dirname)}/migrations/**`)],
     subscribers: ['dist/src/database/subscriber/**'],
   } as DataSourceOptions;
 }
